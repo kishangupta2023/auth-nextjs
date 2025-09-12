71e564a5f72bb549cb923a774e36a72b9d4bb15c
kishan gupta <kishan.2023ug1075@iiitranchi.ac.in>
2025-09-12 19:49:02 +0530

    Refactor email verification state management; change verified and error states to boolean

--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -9,8 +9,8 @@ import React ,{useEffect,useState} from "react";
 
 export default function VerifyEmailPage() {
     const [token, setToken] = useState("");
-    const [verified, setVerified] = useState("");
-    const [error, setError] = useState("");
+    const [verified, setVerified] = useState(false);
+    const [error, setError] = useState(false);
     
     const verifyUserEmail = async ()=>{
         try {
